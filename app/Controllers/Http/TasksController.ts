bb7b0bc0ebe36b3303ac1a0e5573964711b493ff
Jordy Kafwe <jordykafwe@gmail.com>
2020-12-31 19:59:36 +0200

    Create task store route

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -1,4 +1,17 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Task from 'App/Models/Task'
 
 export default class TasksController {
+    public index ({ view }: HttpContextContract) {
+        return view.render('tasks/index')
+    }
+
+    public async store ({ request, response }: HttpContextContract) {
+        await Task.create({
+           title: request.input('title')
+        })
+
+        return response.redirect('back')
+
+    }
 }
