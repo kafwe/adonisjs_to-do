3730ffbf02b887b4a00335761039ef7f18f1b7af
Jordy Kafwe <jordykafwe@gmail.com>
2020-12-31 20:15:31 +0200

    Add form validation

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Task from 'App/Models/Task'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class TasksController {
     public index ({ view }: HttpContextContract) {
@@ -7,8 +8,22 @@ export default class TasksController {
     }
 
     public async store ({ request, response }: HttpContextContract) {
+        const validationSchema = schema.create({
+            title: schema.string({ trim: true }. [
+                rules.maxLength(255), 
+            ])
+        })
+
+        const validatedData = await request.validate({
+            schema: validationSchema, 
+            messages: {
+                'title.required': 'Enter task title',
+                'title.maxLength': 'Task title cannot exceed 255 characters'
+            }, 
+        })
+
         await Task.create({
-           title: request.input('title')
+           title: validatedData.title
         })
 
         return response.redirect('back')
