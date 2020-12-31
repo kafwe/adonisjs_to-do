5015fefbb8218461cc82e2c509d1dac92c1f3879
Jordy Kafwe <jordykafwe@gmail.com>
2020-12-31 17:47:52 +0200

    Add columns to Task model

--- app/Models/Task.ts
+++ app/Models/Task.ts
@@ -5,6 +5,12 @@ export default class Task extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public title: string
+
+  @column()
+  public isCompleted: boolean
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
